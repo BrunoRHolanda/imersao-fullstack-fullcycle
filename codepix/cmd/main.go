c2b81581d1b058ddd68e74379cd16d115d220ddd
Bruno Rodrigues Holanda <bruno.holanda@before.com.br>
2021-02-04 10:27:40 -0400

    [fix: ajustado configuração de porta no servidor gRPC]

--- codepix/cmd/main.go
+++ codepix/cmd/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/BrunoRHolanda/imersao-fullstack-fullcycle/codepix/infrastructure/db"
 	"github.com/BrunoRHolanda/imersao-fullstack-fullcycle/codepix/application/grpc"
+	"github.com/BrunoRHolanda/imersao-fullstack-fullcycle/codepix/infrastructure/db"
 	"github.com/jinzhu/gorm"
 	"os"
 )
@@ -11,4 +11,5 @@ var database *gorm.DB
 
 func main() {
 	database = db.ConnectDB(os.Getenv("env"))
+	grpc.StartGrpcServer(database, 50051)
 }
